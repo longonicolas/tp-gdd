fbc6361023ad112ce8a1d5032e87bac1144714b5
iperedahernandez@frba.utn.edu.ar <iperedahernandez@frba.utn.edu.ar>
2024-10-27 18:31:08 -0300

    TIPOS DE DATOS BIEN! (CREO)

--- scripttp.sql
+++ scripttp.sql
@@ -1,31 +1,53 @@
- /*--para eliminar todas las tabglas y vovler a empezar
--- PASO 1: Eliminar todas las Foreign Keys del esquema LA_NARANJA_MECANICA_V2
+-----------------------
+/* 
+-- PASO 1: Eliminar todas las Foreign Keys del esquema LA_NARANJA_MECANICA_V2 
+*/
 DECLARE @sql NVARCHAR(MAX) = N'';
 
--- Elimina todas las claves foráneas en el esquema
-SELECT @sql += N'ALTER TABLE [' + OBJECT_SCHEMA_NAME(parent_object_id) + '].[' + OBJECT_NAME(parent_object_id) + '] DROP CONSTRAINT [' + name + '];' + CHAR(13)
+-- Genera las instrucciones para eliminar todas las claves foráneas en el esquema
+SELECT @sql += 'ALTER TABLE [' + OBJECT_SCHEMA_NAME(parent_object_id) + '].[' 
+               + OBJECT_NAME(parent_object_id) + '] DROP CONSTRAINT [' + name + '];' + CHAR(13)
 FROM sys.foreign_keys
 WHERE schema_id = SCHEMA_ID('LA_NARANJA_MECANICA_V2');
 
--- Ejecuta el SQL generado
+-- Ejecuta el SQL generado para eliminar las claves foráneas
 EXEC sp_executesql @sql;
 
--- PASO 2: Eliminar todas las tablas del esquema LA_NARANJA_MECANICA_V2
+
+/* 
+-- PASO 2: Eliminar todas las funciones del esquema LA_NARANJA_MECANICA_V2 
+*/
 SET @sql = N'';
 
--- Genera los comandos DROP TABLE para todas las tablas en el esquema
-SELECT @sql += N'DROP TABLE [' + TABLE_SCHEMA + '].[' + TABLE_NAME + '];' + CHAR(13)
+-- Genera las instrucciones para eliminar todas las funciones en el esquema
+SELECT @sql += 'DROP FUNCTION [' + ROUTINE_SCHEMA + '].[' + ROUTINE_NAME + '];' + CHAR(13)
+FROM INFORMATION_SCHEMA.ROUTINES
+WHERE ROUTINE_SCHEMA = 'LA_NARANJA_MECANICA_V2' AND ROUTINE_TYPE = 'FUNCTION';
+
+-- Ejecuta el SQL generado para eliminar las funciones
+EXEC sp_executesql @sql;
+
+
+/* 
+-- PASO 3: Eliminar todas las tablas del esquema LA_NARANJA_MECANICA_V2 
+*/
+SET @sql = N'';
+
+-- Genera las instrucciones para eliminar todas las tablas en el esquema
+SELECT @sql += 'DROP TABLE [' + TABLE_SCHEMA + '].[' + TABLE_NAME + '];' + CHAR(13)
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = 'LA_NARANJA_MECANICA_V2';
 
--- Ejecuta los comandos generados para eliminar las tablas
+-- Ejecuta el SQL generado para eliminar las tablas
 EXEC sp_executesql @sql;
 
--- PASO 3: Eliminar el esquema LA_NARANJA_MECANICA_V2 (opcional)
+DROP FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_localidad
+
+/*
+-- PASO 4: Eliminar el esquema LA_NARANJA_MECANICA_V2 
+*/
 -- Si deseas eliminar el esquema completo, usa esta línea.
 DROP SCHEMA LA_NARANJA_MECANICA_V2;
-
-*/
 
 
 ---------------------CREAMOS ESQUEMA-------------------------
@@ -35,29 +57,29 @@ GO
 
 -- Tabla: cliente
 CREATE TABLE LA_NARANJA_MECANICA_V2.cliente(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	nombre VARCHAR(255),
-	apellido VARCHAR(255),
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	nombre NVARCHAR(50),
+	apellido NVARCHAR(50),
 	fecha_nacimiento DATE,
-	mail VARCHAR(150),
-	dni VARCHAR(50)
+	mail NVARCHAR(50),
+	dni decimal(18,0)
 )
 
 CREATE TABLE LA_NARANJA_MECANICA_V2.vendedor(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	razon_social VARCHAR(255),
-	cuit VARCHAR(12),
-	mail VARCHAR(150),
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	razon_social NVARCHAR(50),
+	cuit NVARCHAR(50),
+	mail NVARCHAR(50),
 )
 
 
 CREATE TABLE LA_NARANJA_MECANICA_V2.usuario(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	usuario VARCHAR(255),
-	password VARCHAR(255),
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	usuario NVARCHAR(50),
+	password NVARCHAR(50),
 	fecha_creacion DATE,
-	id_vendedor INTEGER,
-	id_cliente INTEGER,
+	id_vendedor decimal(18,0),
+	id_cliente decimal(18,0),
 	FOREIGN KEY (id_vendedor) REFERENCES LA_NARANJA_MECANICA_V2.vendedor(id),
 	FOREIGN KEY (id_cliente) REFERENCES LA_NARANJA_MECANICA_V2.cliente(id)
 )
@@ -65,67 +87,107 @@ CREATE TABLE LA_NARANJA_MECANICA_V2.usuario(
 
 -- Tabla: provincia
 CREATE TABLE LA_NARANJA_MECANICA_V2.provincia(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	nombre VARCHAR(255)
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	nombre NVARCHAR(50)
 )
 
 CREATE TABLE LA_NARANJA_MECANICA_V2.localidad(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_provincia INTEGER,
-	nombre VARCHAR(255),
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	id_provincia decimal(18,0),
+	nombre NVARCHAR(50),
 	FOREIGN KEY (id_provincia) REFERENCES LA_NARANJA_MECANICA_V2.provincia(id)
 )
 
 CREATE TABLE LA_NARANJA_MECANICA_V2.domicilio(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_localidad INTEGER,
-	id_usuario INTEGER,
-	calle VARCHAR(255),
-	nro_calle SMALLINT,
-	piso SMALLINT,
-	depto VARCHAR(3),
-	codigo_postal VARCHAR(4),
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	id_localidad decimal(18,0),
+	id_usuario decimal(18,0),
+	calle NVARCHAR(50),
+	nro_calle decimal(18,0),
+	piso decimal(18,0),
+	depto NVARCHAR(50),
+	codigo_postal decimal(18,0),
 	FOREIGN KEY (id_localidad) REFERENCES LA_NARANJA_MECANICA_V2.localidad(id),
 	FOREIGN KEY (id_usuario) REFERENCES LA_NARANJA_MECANICA_V2.usuario(id)
 )
 
 CREATE TABLE LA_NARANJA_MECANICA_V2.almacen(
-	codigo_almacen INTEGER PRIMARY KEY,
-	costo_al_dia DECIMAL(10,2),
-	id_domicilio INTEGER
+	codigo_almacen decimal(18,0) PRIMARY KEY,
+	costo_al_dia DECIMAL(18,2),
+	id_domicilio decimal(18,0)
 	FOREIGN KEY (id_domicilio) REFERENCES LA_NARANJA_MECANICA_V2.domicilio(id)
 )
+
+CREATE TABLE LA_NARANJA_MECANICA_V2.marca(
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	nombre NVARCHAR(50)
+)
+
+CREATE TABLE LA_NARANJA_MECANICA_V2.rubro(
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	descripcion NVARCHAR(50)
+)
+
+CREATE TABLE LA_NARANJA_MECANICA_V2.subrubro(
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	id_rubro decimal(18,0),
+	nombre NVARCHAR(50),
+	FOREIGN KEY (id_rubro) REFERENCES LA_NARANJA_MECANICA_V2.rubro(id)
+)
+
+-- Tabla: modelo_producto
+CREATE TABLE LA_NARANJA_MECANICA_V2.modeloProducto (
+    codigo_modelo decimal(18,0) PRIMARY KEY,
+    descripcion NVARCHAR(50)
+);
+
+-- Tabla: producto. Tabla maestra tiene producto_precio y aca no lo tenemos
+CREATE TABLE LA_NARANJA_MECANICA_V2.producto(
+	id decimal(18,0) IDENTITY(1,1) PRIMARY KEY,
+	codigo_producto NVARCHAR(50),
+	descripcion NVARCHAR(50),
+	precio decimal(18,2),
+	codigo_modelo decimal(18,0),
+	id_marca decimal(18,0),
+	id_subrubro decimal(18,0),
+	FOREIGN KEY (codigo_modelo) REFERENCES LA_NARANJA_MECANICA_V2.modeloProducto(codigo_modelo),
+	FOREIGN KEY (id_marca) REFERENCES LA_NARANJA_MECANICA_V2.marca(id),
+	FOREIGN KEY (id_subrubro) REFERENCES LA_NARANJA_MECANICA_V2.subrubro(id)
+)
+
 -- Tabla: publicacion
 CREATE TABLE LA_NARANJA_MECANICA_V2.publicacion (
-    codigo_publicacion BIGINT PRIMARY KEY,
-    descripcion TEXT,
+    codigo_publicacion decimal(18,0) PRIMARY KEY,
+    descripcion NVARCHAR(50),
     fecha_inicio DATE,
     fecha_fin DATE,
-    id_producto BIGINT,
-    stock SMALLINT,
+    id_producto decimal(18,0),
+    stock decimal(18,0),
     precio DECIMAL(18, 2),
-    id_usuario INTEGER,
-    id_almacen INTEGER,
+    id_usuario decimal(18,0),
+    id_almacen decimal(18,0),
     costo DECIMAL(18, 2),
     porcentaje_por_venta DECIMAL(18, 2),
     FOREIGN KEY (id_usuario) REFERENCES LA_NARANJA_MECANICA_V2.usuario(id),
-    FOREIGN KEY (id_almacen) REFERENCES LA_NARANJA_MECANICA_V2.almacen(codigo_almacen)
-);
+    FOREIGN KEY (id_almacen) REFERENCES LA_NARANJA_MECANICA_V2.almacen(codigo_almacen),
+	FOREIGN KEY (id_producto) REFERENCES LA_NARANJA_MECANICA_V2.producto(id)
+)
 
 -- Tabla: detalle_venta
 CREATE TABLE LA_NARANJA_MECANICA_V2.detalle_venta (
-    id_detalle_venta BIGINT PRIMARY KEY,
-    id_publicacion BIGINT,
-    cantidad SMALLINT,-- en la tabla maestra esta como DECIMAL(18, 0) y tambien esta el precio que no lo pusimos aca
+    id_detalle_venta decimal(18,0) PRIMARY KEY,
+    id_publicacion decimal(18,0),
+    cantidad decimal(18,0),
+	precio decimal(18,2),-- en la tabla maestra esta como DECIMAL(18, 0) y tambien esta el precio que no lo pusimos aca
     subtotal DECIMAL(18, 2),
     FOREIGN KEY (id_publicacion) REFERENCES LA_NARANJA_MECANICA_V2.publicacion(codigo_publicacion)
 );
 
 -- Tabla: venta
 CREATE TABLE LA_NARANJA_MECANICA_V2.venta (
-    nro_venta BIGINT PRIMARY KEY,
-    id_usuario INTEGER,
-    id_detalle_venta BIGINT,
+    nro_venta decimal(18,0) PRIMARY KEY, --VENTA_CODIGO DE LA TABLA MAESTRA
+    id_usuario decimal(18,0),
+    id_detalle_venta decimal(18,0),
     fecha DATE,
     total DECIMAL(18, 2),
     FOREIGN KEY (id_usuario) REFERENCES LA_NARANJA_MECANICA_V2.usuario(id),
@@ -134,21 +196,21 @@ CREATE TABLE LA_NARANJA_MECANICA_V2.venta (
 
 -- Tabla: tipo_envio
 CREATE TABLE LA_NARANJA_MECANICA_V2.tipo_envio (
-    id_tipo_envio BIGINT PRIMARY KEY,
+    id_tipo_envio decimal(18,0) PRIMARY KEY,
     nombre NVARCHAR(50)
 );
 
 -- Tabla: envio
 CREATE TABLE LA_NARANJA_MECANICA_V2.envio (
-    nro_envio BIGINT PRIMARY KEY,
-    nro_venta BIGINT,
-    id_domicilio INTEGER,
+    nro_envio decimal(18,0) PRIMARY KEY,
+    nro_venta decimal(18,0),
+    id_domicilio decimal(18,0),
     fecha DATE,
-    hora_inicio NVARCHAR(5),--DECIMAL(18, 0) en tabla maestra
-    hora_fin NVARCHAR(5),--DECIMAL(18, 0) en tabla maestra
+    hora_inicio decimal(18,0),
+    hora_fin decimal(18,0),
     costo DECIMAL(18, 2),
     fecha_entrega DATETIME,
-    id_tipo_envio BIGINT,
+    id_tipo_envio decimal(18,0),
     FOREIGN KEY (nro_venta) REFERENCES LA_NARANJA_MECANICA_V2.venta(nro_venta),
     FOREIGN KEY (id_domicilio) REFERENCES LA_NARANJA_MECANICA_V2.domicilio(id),
     FOREIGN KEY (id_tipo_envio) REFERENCES LA_NARANJA_MECANICA_V2.tipo_envio(id_tipo_envio)
@@ -156,27 +218,28 @@ CREATE TABLE LA_NARANJA_MECANICA_V2.envio (
 
 -- Tabla: concepto
 CREATE TABLE LA_NARANJA_MECANICA_V2.concepto (
-    id_concepto BIGINT PRIMARY KEY,
+    id_concepto decimal(18,0) PRIMARY KEY,
     nombre NVARCHAR(50)
 );
 
 -- Tabla: detalle_factura
 CREATE TABLE LA_NARANJA_MECANICA_V2.detalle_factura (
-    id_detalle_factura BIGINT PRIMARY KEY,
-    codigo_publicacion BIGINT,
-    id_concepto BIGINT,--tabla maestra esta detalle_sub_total que no lo pusimos nosotros
-    cantidad SMALLINT,--DECIMAL(18, 2) en tabla maestra
+    id_detalle_factura decimal(18,0) PRIMARY KEY,
+    codigo_publicacion decimal(18,0),
+    id_concepto decimal(18,0), --FACTURA_DET_TIPÖ
+    cantidad decimal(18,0),
     precio_unitario DECIMAL(18, 2),
+	detalle_subtotal decimal(18,2),
     FOREIGN KEY (codigo_publicacion) REFERENCES LA_NARANJA_MECANICA_V2.publicacion(codigo_publicacion),
     FOREIGN KEY (id_concepto) REFERENCES LA_NARANJA_MECANICA_V2.concepto(id_concepto)
 );
 
 -- Tabla: factura
 CREATE TABLE LA_NARANJA_MECANICA_V2.factura (
-    nro_factura BIGINT PRIMARY KEY,
+    nro_factura decimal(18,0) PRIMARY KEY, --FACTURA_NUMERO en tabla maestra
     fecha DATE,
-    id_usuario INTEGER,
-    id_detalle_factura BIGINT,
+    id_usuario decimal(18,0),
+    id_detalle_factura decimal(18,0),
     total DECIMAL(18, 2),
     FOREIGN KEY (id_usuario) REFERENCES LA_NARANJA_MECANICA_V2.usuario(id),
     FOREIGN KEY (id_detalle_factura) REFERENCES LA_NARANJA_MECANICA_V2.detalle_factura(id_detalle_factura)
@@ -184,16 +247,17 @@ CREATE TABLE LA_NARANJA_MECANICA_V2.factura (
 
 -- Tabla: tipo_medio_pago
 CREATE TABLE LA_NARANJA_MECANICA_V2.tipo_medio_pago (
-    id_tipo_medio_pago BIGINT PRIMARY KEY,
+    id_tipo_medio_pago decimal(18,0) PRIMARY KEY,
     nombre NVARCHAR(50)
 );
 
 -- Tabla: medio_pago
 CREATE TABLE LA_NARANJA_MECANICA_V2.medio_pago (
-    id_medio_pago BIGINT PRIMARY KEY,
+    id_medio_pago decimal(18,0) PRIMARY KEY,
     nro_tarjeta NVARCHAR(50),
     fecha_vencimiento DATE,
-    id_tipo_medio_pago BIGINT,
+    id_tipo_medio_pago decimal(18,0),
+	nombre_medio_pago NVARCHAR(50),
     FOREIGN KEY (id_tipo_medio_pago) REFERENCES LA_NARANJA_MECANICA_V2.tipo_medio_pago(id_tipo_medio_pago)
 );
 
@@ -201,57 +265,22 @@ CREATE TABLE LA_NARANJA_MECANICA_V2.medio_pago (
 
 -- Tabla: pago
 CREATE TABLE LA_NARANJA_MECANICA_V2.pago (
-    nro_pago BIGINT PRIMARY KEY,
-    nro_venta BIGINT,
-    id_medio_pago BIGINT,
+    nro_pago decimal(18,0) PRIMARY KEY,
+    nro_venta decimal(18,0),
+    id_medio_pago decimal(18,0),
     importe DECIMAL(18, 2),
     fecha DATE,
-    cuotas SMALLINT,--DECIMAL(18, 0) en tabla maestra
+    cuotas decimal(18,0),--DECIMAL(18, 0) en tabla maestra
     FOREIGN KEY (nro_venta) REFERENCES LA_NARANJA_MECANICA_V2.venta(nro_venta),
     FOREIGN KEY (id_medio_pago) REFERENCES LA_NARANJA_MECANICA_V2.medio_pago(id_medio_pago)
 );
 
-CREATE TABLE LA_NARANJA_MECANICA_V2.marca(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	nombre VARCHAR(255)
-)
-
-CREATE TABLE LA_NARANJA_MECANICA_V2.rubro(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	descripcion TEXT
-)
-
-CREATE TABLE LA_NARANJA_MECANICA_V2.subrubro(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	id_rubro INTEGER,
-	nombre VARCHAR(255),
-	FOREIGN KEY (id_rubro) REFERENCES LA_NARANJA_MECANICA_V2.rubro(id)
-)
-
--- Tabla: modelo_producto
-CREATE TABLE LA_NARANJA_MECANICA_V2.modeloProducto (
-    codigo_modelo INTEGER PRIMARY KEY,
-    descripcion TEXT
-);
-
--- Tabla: producto. Tabla maestra tiene producto_precio y aca no lo tenemos
-CREATE TABLE LA_NARANJA_MECANICA_V2.producto(
-	id INTEGER IDENTITY(1,1) PRIMARY KEY,
-	codigo_producto VARCHAR(255),
-	descripcion TEXT,
-	codigo_modelo INTEGER,
-	id_marca INTEGER,
-	id_subrubro INTEGER,
-	FOREIGN KEY (codigo_modelo) REFERENCES LA_NARANJA_MECANICA_V2.modeloProducto(codigo_modelo),
-	FOREIGN KEY (id_marca) REFERENCES LA_NARANJA_MECANICA_V2.marca(id),
-	FOREIGN KEY (id_subrubro) REFERENCES LA_NARANJA_MECANICA_V2.subrubro(id)
-)
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.get_id_rubro(@descripcion TEXT)
-RETURNS INTEGER
+CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_rubro(@descripcion NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.rubro
@@ -264,11 +293,11 @@ GO
 
 -- OBTENER ID DE UN SUBRUBRO
 GO
-CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_subrubro(@subrubro VARCHAR(255), @rubro VARCHAR(255))
-RETURNS INTEGER
+CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_subrubro(@subrubro NVARCHAR(50), @rubro NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.subrubro
@@ -279,11 +308,11 @@ GO
 -- OBTENER ID DE UNA MARCA
 	
 GO
-CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_marca(@marca VARCHAR(255))
-RETURNS INTEGER
+CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_marca(@marca NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.marca
@@ -294,11 +323,11 @@ END
 GO
 	-- OBTENER UN ID DE UN PRODUCTO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.get_id_producto(@codigo VARCHAR(255), @descripcion VARCHAR(255), @marca VARCHAR(255), @modelo INTEGER,@subrubro VARCHAR(255), @rubro VARCHAR(255))
-RETURNS INTEGER
+CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.get_id_producto(@codigo NVARCHAR(50), @descripcion NVARCHAR(50), @marca NVARCHAR(50), @modelo decimal(18,0),@subrubro NVARCHAR(50), @rubro NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 	
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.producto p
@@ -312,11 +341,11 @@ BEGIN
 END
 GO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_cliente(@nombre VARCHAR(255), @apellido VARCHAR(255), @dni VARCHAR(50))
-RETURNS INTEGER
+CREATE OR ALTER FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_cliente(@nombre NVARCHAR(50), @apellido NVARCHAR(50), @dni decimal(18,0))
+RETURNS decimal(18,0)
 AS 
 BEGIN
-    DECLARE @id INTEGER;
+    DECLARE @id decimal(18,0);
 
     SET @id = -1;
 
@@ -327,14 +356,14 @@ BEGIN
       AND dni = @dni;
 
     RETURN @id;
-END;
+END
 GO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_vendedor(@cuit VARCHAR(12), @razonSocial VARCHAR(255))
-RETURNS INTEGER
+CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_vendedor(@cuit NVARCHAR(50), @razonSocial NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.vendedor
@@ -344,11 +373,11 @@ BEGIN
 END
 GO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_usuario_cliente(@username VARCHAR(255), @password VARCHAR(255), @fecha_creacion DATE)
-RETURNS INTEGER
+CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_usuario_cliente(@username NVARCHAR(50), @password NVARCHAR(50), @fecha_creacion DATE)
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.usuario
@@ -358,11 +387,11 @@ BEGIN
 END
 GO
 GO
-CREATE FUNCTION  LA_NARANJA_MECANICA_V2.devolver_id_localidad(@nombre VARCHAR(255), @provincia VARCHAR(255))
-RETURNS INTEGER
+CREATE FUNCTION  LA_NARANJA_MECANICA_V2.devolver_id_localidad(@nombre NVARCHAR(50), @provincia NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = l.id
 	FROM LA_NARANJA_MECANICA_V2.localidad l
@@ -373,11 +402,11 @@ BEGIN
 END
 GO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.get_id_domicilio(@calle VARCHAR(255), @altura SMALLINT, @localidad VARCHAR(255), @provincia VARCHAR(255))
-RETURNS INTEGER
+CREATE FUNCTION LA_NARANJA_MECANICA_V2.get_id_domicilio(@calle NVARCHAR(50), @altura decimal(18,0), @localidad NVARCHAR(50), @provincia NVARCHAR(50))
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.domicilio
@@ -387,11 +416,11 @@ BEGIN
 END
 GO
 GO
-CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_usuario_vendedor(@username VARCHAR(255), @password VARCHAR(255), @fecha_creacion DATE)
-RETURNS INTEGER
+CREATE FUNCTION LA_NARANJA_MECANICA_V2.devolver_id_usuario_vendedor(@username NVARCHAR(50), @password NVARCHAR(50), @fecha_creacion DATE)
+RETURNS decimal(18,0)
 AS
 BEGIN
-	DECLARE @id INTEGER
+	DECLARE @id decimal(18,0)
 
 	SELECT @id = id
 	FROM LA_NARANJA_MECANICA_V2.usuario
@@ -522,3 +551,4 @@ SELECT DISTINCT PUBLICACION_CODIGO, PUBLICACION_DESCRIPCION, PUBLICACION_STOCK, 
 				LA_NARANJA_MECANICA_V2.get_id_producto(PRODUCTO_CODIGO, PRODUCTO_DESCRIPCION, PRODUCTO_MARCA, PRODUCTO_MOD_CODIGO, PRODUCTO_SUB_RUBRO, PRODUCTO_RUBRO_DESCRIPCION), ALMACEN_CODIGO
 FROM gd_esquema.Maestra
 WHERE VEN_USUARIO_NOMBRE IS NOT NULL AND PUBLICACION_CODIGO IS NOT NULL
+
